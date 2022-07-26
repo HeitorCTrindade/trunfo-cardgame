53a031dbd73a561a1fdfcd19b411b527254bfa8a
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-26 14:42:54 -0300

    Requesito 02 em andamento.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,34 +3,97 @@ import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
-    
-    const 
-    
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <div>
         <h2>Adicionar nova carta</h2>
         <form className="form-cards">
           <p>Nome:</p>
-          <input type="text" name="" id="" data-testid="name-input" />
+          <input
+            type="text"
+            name=""
+            id=""
+            data-testid="name-input"
+            value={ cardName }
+          />
           <p>Descrição:</p>
-          <textarea name="" id="" data-testid="description-input" />
+          <textarea
+            name=""
+            id=""
+            data-testid="description-input"
+            value={ cardDescription }
+          />
           <p>attr01</p>
-          <input type="number" name="" id="" data-testid="attr1-input" />
+          <input
+            type="number"
+            name=""
+            id=""
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+          />
           <p>attr02</p>
-          <input type="number" name="" id="" data-testid="attr2-input" />
+          <input
+            type="number"
+            name=""
+            id=""
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+          />
           <p>attr03</p>
-          <input type="number" name="" id="" data-testid="attr3-input" />
+          <input
+            type="number"
+            name=""
+            id=""
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+          />
           <p>Imagem:</p>
-          <input type="text" name="" id="" data-testid="image-input" />
+          <input
+            type="text"
+            name=""
+            id=""
+            data-testid="image-input"
+            value={ cardImage }
+          />
           <p>Selecione a raridade da carta:</p>
-          <select name="cardRare" id="" data-testid="rare-input">
+          <select
+            name="cardRare"
+            id=""
+            data-testid="rare-input"
+            value={ cardRare }
+          >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
             <option value="muito raro">muito raro</option>
           </select>
-          <input type="checkbox" name="" id="trunfo" data-testid="trunfo-input" />
+          <input
+            type="checkbox"
+            name=""
+            id="trunfo"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+          />
           Super Trybe Trunfo
-          <button data-testid="save-button">Salvar</button>
+          <button
+            disabled={ isSaveButtonDisabled }
+            data-testid="save-button"
+            onClick={ onSaveButtonClick }
+          >
+            Salvar
+          </button>
         </form>
       </div>
     );
