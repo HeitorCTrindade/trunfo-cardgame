d11ba68f29eec0788913b3e92917aee5c9f28af9
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-27 16:12:41 -0300

    Requisitos revisados - checkbox funcionando - Css basico da carta implementado.

--- src/App.js
+++ src/App.js
@@ -25,35 +25,36 @@ class App extends React.Component {
 
   handleChangesForm(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value }, () => {
-      // if (name === 'cardTrunfo') {
-      //   this.setState((prevState) => ({ [name]: !prevState.cardTrunfo }));
-      // }
-      const limitSumAtrr = 210;
-      const limitAtrr = 90;
-      const {
-        cardName,
-        cardDescription,
-        cardAttr1,
-        cardAttr2,
-        cardAttr3,
-        cardImage,
-        cardRare,
-      } = this.state;
-      const cardAttr1Int = parseInt(cardAttr1, 10);
-      const cardAttr2Int = parseInt(cardAttr2, 10);
-      const cardAttr3Int = parseInt(cardAttr3, 10);
-      const isButtonDisabled = cardName.length === 0
-      || cardDescription.length === 0
-      || cardRare.length === 0
-      || cardImage.length === 0
-      || (cardAttr1Int + cardAttr2Int + cardAttr3Int) > limitSumAtrr
+    if (name === 'cardTrunfo') {
+      this.setState((prevState) => ({ [name]: !prevState.cardTrunfo }));
+    } else {
+      this.setState({ [name]: value }, () => {
+        const limitSumAtrr = 210;
+        const limitAtrr = 90;
+        const {
+          cardName,
+          cardDescription,
+          cardAttr1,
+          cardAttr2,
+          cardAttr3,
+          cardImage,
+          cardRare,
+        } = this.state;
+        const cardAttr1Int = parseInt(cardAttr1, 10);
+        const cardAttr2Int = parseInt(cardAttr2, 10);
+        const cardAttr3Int = parseInt(cardAttr3, 10);
+        const isButtonDisabled = cardName.length === 0
+        || cardDescription.length === 0
+        || cardRare.length === 0
+        || cardImage.length === 0
+        || (cardAttr1Int + cardAttr2Int + cardAttr3Int) > limitSumAtrr
       || cardAttr1Int > limitAtrr || cardAttr2Int > limitAtrr || cardAttr3Int > limitAtrr
-      || cardAttr1Int < 0 || cardAttr2Int < 0 || cardAttr3Int < 0;
-      this.setState({
-        isSaveButtonDisabled: isButtonDisabled,
+        || cardAttr1Int < 0 || cardAttr2Int < 0 || cardAttr3Int < 0;
+        this.setState({
+          isSaveButtonDisabled: isButtonDisabled,
+        });
       });
-    });
+    }
   }
 
   handleSaveButton = (event) => {
@@ -86,6 +87,7 @@ class App extends React.Component {
           cardAttr2: 0,
           cardAttr3: 0,
           hasTrunfo: true,
+          cardTrunfo: false,
         };
       }
       return {
@@ -97,6 +99,7 @@ class App extends React.Component {
         cardAttr1: 0,
         cardAttr2: 0,
         cardAttr3: 0,
+        cardTrunfo: false,
       };
     });
   }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -20,30 +20,37 @@ class Card extends Component {
       renderTrunfo = <h3> </h3>;
     }
     return (
-      <section>
-        <h3 data-testid="name-card">
+      <section className="card">
+        <h3 className="container" data-testid="name-card">
           { cardName }
         </h3>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <p data-testid="description-card">
-          { cardDescription }
-        </p>
-        <p data-testid="attr1-card">
-          atrrb01.............
-          { cardAttr1 }
-        </p>
-        <p data-testid="attr2-card">
-          atrrb02.............
-          { cardAttr2 }
-        </p>
-        <p data-testid="attr3-card">
-          atrrb03.............
-          { cardAttr3 }
-        </p>
-        <h3 data-testid="rare-card">
-          { cardRare }
-        </h3>
-        { renderTrunfo }
+        <img
+          className="container"
+          src={ cardImage }
+          alt={ cardName }
+          data-testid="image-card"
+        />
+        <div className="container">
+          <p data-testid="description-card">
+            { cardDescription }
+          </p>
+          <p data-testid="attr1-card">
+            atrrb01.............
+            { cardAttr1 }
+          </p>
+          <p data-testid="attr2-card">
+            atrrb02.............
+            { cardAttr2 }
+          </p>
+          <p data-testid="attr3-card">
+            atrrb03.............
+            { cardAttr3 }
+          </p>
+          <h3 data-testid="rare-card">
+            { cardRare }
+          </h3>
+          { renderTrunfo }
+        </div>
       </section>
     );
   }
